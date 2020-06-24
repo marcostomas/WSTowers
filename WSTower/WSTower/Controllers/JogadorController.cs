3e676979902d99b3797d8db9bbc2eda3e7ea48e0
Thiago Henrique <henriquesilva322@outlook.com>
2020-06-24 20:34:37 -0300

    Lista jogadores

---
+++ WSTower/WSTower/Controllers/JogadorController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSTower.Domains;
+using WSTower.Interfaces;
+using WSTower.Repositories;
+
+namespace WSTower.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class JogadorController : ControllerBase
+    {
+        private IJogadorRepository _jogadorRepository;
+        public JogadorController()
+        {
+            _jogadorRepository = new JogadorRepository();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                Jogador jogadorBuscado = _jogadorRepository.BuscarPorId(id);
+
+                if(jogadorBuscado != null)
+                {
+                    return Ok(jogadorBuscado);
+                }
+
+                return NotFound("nenhum jogador encontrado");
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+            
+        }
+
+      
+
+    }
+}
